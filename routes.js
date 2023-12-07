f723ce603dc4a4f2af0b5e70d7048e0866ab5326
surya-10 <sp659151@gmail.com>
2023-12-07 23:29:57 +0530

    done

--- routes.js
+++ routes.js
@@ -30,8 +30,8 @@ imagesRouter.post("/booking/payment-page/user-pay/:id", async (req, res) => {
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: "payment",
-            success_url: "http://localhost:3000/success",
-            cancel_url: "http://localhost:3000/cancel"
+            success_url: "https://master--zippy-mermaid-f891a4.netlify.app/success",
+            cancel_url: "https://master--zippy-mermaid-f891a4.netlify.app/cancel"
         });
 
         return res.json({
